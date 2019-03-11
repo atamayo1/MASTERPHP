8fa64c5fdb3bae420a20e1461ee16f6b71ffba8a
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-03-10 22:45:24 -0500

    Bucle and IF

--- 11-bucles/index.php
+++ 11-bucles/index.php
@@ -2,7 +2,25 @@
 
 /* BUCLE WHILE
 Estructura de control que itera o repite la ejecución de una serie de instrucciones
-tantas veces como sea necesario.
+tantas veces como sea necesario, En base a una condición.
+
+while(condiciones){
+    bloque de instrucciones
+    otra instrucción
+}
+
 */
+$numero = 0;
+
+while($numero <= 100){
+
+    if($numero == 100){
+        echo "$numero";
+    }else{
+        echo "$numero, ";
+    }
+    
+    $numero++;
+}
 
 ?>
